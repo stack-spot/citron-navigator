542d538d6d68f9d571d532d79b7a9053abd14f48
Tiago Peres França <tiago.franca@zup.com.br>
2024-04-16 11:15:14 -0300

    cli tests

--- packages/cli/test/utils.ts
+++ packages/cli/test/utils.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-console */
+
+import { readFile } from 'fs/promises'
 import { ConfigParser } from '../src/ConfigParser'
+import { generate } from '../src/generate'
 
 export function expectToFail() {
   expect(true).toBe(false)
@@ -11,4 +15,43 @@ export function expectToThrowWhenParsing(yaml: string, expectedErrorClass: { new
   } catch (error: any) {
     expect(error instanceof expectedErrorClass).toBe(true)
   }
+}
+
+export async function expectToGenerateCode(src: string, out: string) {
+  const { exitMock, unMockExit } = mockExit()
+  await generate({ src, out })
+  expect(exitMock).toHaveBeenCalledWith(0)
+  unMockExit()
+  const content = await readFile(out, { encoding: 'utf-8' })
+  expect(content).toMatchSnapshot()
+}
+
+export function mockExit() {
+  const exit = process.exit
+  const exitMock = jest.fn()
+  process.exit = exitMock as unknown as typeof exit
+  const unMockExit = () => process.exit = exit
+  return { exitMock, unMockExit }
+}
+
+export function mockConsoleLogs() {
+  const original = {
+    log: console.log,
+    warn: console.warn,
+    error: console.error,
+  }
+  const consoleMock = {
+    log: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+  }
+  console.log = consoleMock.log
+  console.warn = consoleMock.warn
+  console.error = consoleMock.error
+  const unMockConsoleLogs = () => {
+    console.log = original.log
+    console.warn = original.warn
+    console.error = original.error
+  }
+  return { consoleMock, unMockConsoleLogs }
 }
