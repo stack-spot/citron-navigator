aad17e46af104ad443d2ab346051f2c58360c450
Tiago Peres França <tiago.franca@zup.com.br>
2024-05-10 12:05:10 -0300

    navigation list label factory can now return undefined and nul

--- packages/runtime/src/use-navigation-list.ts
+++ packages/runtime/src/use-navigation-list.ts
@@ -2,6 +2,12 @@ import { useEffect, useState } from 'react'
 import { CitronNavigator } from './CitronNavigator'
 import { AnyRoute } from './Route'
 
+interface NavigationItem {
+  key: string,
+  href: string,
+  label: string,
+}
+
 function routeKeyToLabel(key: string) {
   const keyParts = key.split('.')
   const lastKeyPart = keyParts[keyParts.length - 1]
@@ -15,9 +21,13 @@ function routeKeyToLabel(key: string) {
  * The return value is a list where each item contains: key (unique id), label (name) and href (url).
  * 
  * @param labelFactory optional. A function to create a label for a route. If not provided, every route will have the last part of
- * its key capitalized as its label.
+ * its key capitalized as its label. If the label factory returns undefined, the default rule for building labels is used. If it returns
+ * null, the route is removed from the list.
+ * @returns the navigation list.
  */
-export function useNavigationList(labelFactory?: (key: string, params: Record<string, any>) => string) {
+export function useNavigationList(
+  labelFactory?: (key: string, params: Record<string, any>) => string | undefined | null,
+): NavigationItem[] {
   const [data, setData] = useState<{ route?: AnyRoute, params?: Record<string, any> }>({
     route: CitronNavigator.instance?.currentRoute,
     params: CitronNavigator.instance?.currentParams,
@@ -27,9 +37,17 @@ export function useNavigationList(labelFactory?: (key: string, params: Record<st
 
   if (!data.route) return []
   const branch = data.route.$getBranch()
-  return branch.map(r => ({
-    key: r.$key,
-    href: r.$link({}),
-    label: labelFactory ? labelFactory(r.$key, data.params ?? {}) : routeKeyToLabel(r.$key),
-  }))
+  return branch.reduce(
+    (result, route) => {
+      let label = labelFactory ? labelFactory(route.$key, data.params ?? {}) : routeKeyToLabel(route.$key)
+      if (label === undefined) label = routeKeyToLabel(route.$key)
+      return label === null
+        ? result
+        : [
+          ...result,
+          { key: route.$key, href: route.$link({}), label },
+        ]
+    },
+    [] as NavigationItem[],
+  )
 }
